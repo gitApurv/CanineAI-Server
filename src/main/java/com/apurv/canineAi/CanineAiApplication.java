1baf9f7915a9a0a5c512b85b55c070877d2b9349
Apurv Maurya <apurvmaurya5@gmail.com>
2026-02-28 19:28:26 +0530

    Initialize project structure with Maven, including wrapper scripts and basic Spring Boot application setup

---
+++ src/main/java/com/apurv/canineAi/CanineAiApplication.java
@@ -0,0 +1,13 @@
+package com.apurv.canineAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanineAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanineAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apurv/canineAi/CanineAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apurv.canineAi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanineAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
